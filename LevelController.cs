091f0cfed7825c1443b87112c622faf9fcde1f4a
John McBride <jgm23333@gmail.com>
2020-10-21 11:23:10 -0600

    Added Level0 for introduction

--- LevelController.cs
+++ LevelController.cs
@@ -34,12 +34,12 @@ public class LevelController : MonoBehaviour
     };
 
   
-    private static int _nextLevelIndex = 1;
+    private static int _nextLevelIndex = 0;
     private const int _maxLevel = 4;
     private int _clock_period = 0;
     private static int _totalCoins = 0;
     private int _centiCoinsThisLevel = 10000;
-    private Text tc_text, cc_text, glitch_text;
+    private Text tc_text, cc_text, glitch_text, intro_text;
     private bool _scoreAdded = false;
 
     public void AdvanceClock()
@@ -78,60 +78,82 @@ public class LevelController : MonoBehaviour
         myGO.AddComponent<GraphicRaycaster>();
         // Debug.Log("myGO.transform = " + myGO.transform);
 
-        // Text
-        myText = new GameObject();
-        myText.transform.parent = myGO.transform;
-        myText.name = "TotalCoins";
-        // Debug.Log("mytext.transform = " + myText.transform);
+        if (_nextLevelIndex != 0)
+        {
+            // Text
+            myText = new GameObject();
+            myText.transform.parent = myGO.transform;
+            myText.name = "TotalCoins";
+            // Debug.Log("mytext.transform = " + myText.transform);
 
-        tc_text = myText.AddComponent<Text>();
-        tc_text.font = (Font)Resources.Load("Anton");
-        tc_text.text = "Total Coins:  " + _totalCoins;
-        tc_text.fontSize = 30;
-        // Debug.Log("tc_text.text = " + tc_text.text);
+            tc_text = myText.AddComponent<Text>();
+            tc_text.font = (Font)Resources.Load("Anton");
+            tc_text.text = "Total Coins:  " + _totalCoins;
+            tc_text.fontSize = 30;
+            // Debug.Log("tc_text.text = " + tc_text.text);
 
-        // Text position
-        rectTransform = tc_text.GetComponent<RectTransform>();
-        rectTransform.localPosition = new Vector3(19.3f, -8.7f, 0);
-        rectTransform.sizeDelta = new Vector2(400, 200);
-        rectTransform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+            // Text position
+            rectTransform = tc_text.GetComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(19.3f, -8.7f, 0);
+            rectTransform.sizeDelta = new Vector2(400, 200);
+            rectTransform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
 
-        // Text
-        myText = new GameObject();
-        myText.transform.parent = myGO.transform;
-        myText.name = "CurrentCoins";
-        // Debug.Log("mytext.transform = " + myText.transform);
+            // Text
+            myText = new GameObject();
+            myText.transform.parent = myGO.transform;
+            myText.name = "CurrentCoins";
+            // Debug.Log("mytext.transform = " + myText.transform);
 
-        cc_text = myText.AddComponent<Text>();
-        cc_text.font = (Font)Resources.Load("Anton");
-        cc_text.text = "Current Coins:  " + ((int)((float)_centiCoinsThisLevel / 100.0));
-        cc_text.fontSize = 30;
-        // Debug.Log("text.text = " + cc_text.text);
+            cc_text = myText.AddComponent<Text>();
+            cc_text.font = (Font)Resources.Load("Anton");
+            cc_text.text = "Current Coins:  " + ((int)((float)_centiCoinsThisLevel / 100.0));
+            cc_text.fontSize = 30;
+            // Debug.Log("text.text = " + cc_text.text);
 
-        // Text position
-        rectTransform = cc_text.GetComponent<RectTransform>();
-        rectTransform.localPosition = new Vector3(19.3f, -9.4f, 0);
-        rectTransform.sizeDelta = new Vector2(400, 200);
-        rectTransform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+            // Text position
+            rectTransform = cc_text.GetComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(19.3f, -9.4f, 0);
+            rectTransform.sizeDelta = new Vector2(400, 200);
+            rectTransform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
 
-        // Glitch Text
-        myText = new GameObject();
-        myText.transform.parent = myGO.transform;
-        myText.name = "GlitchAdvisement";
-        // Debug.Log("mytext.transform = " + myText.transform);
+            // Glitch Text
+            myText = new GameObject();
+            myText.transform.parent = myGO.transform;
+            myText.name = "GlitchAdvisement";
+            // Debug.Log("mytext.transform = " + myText.transform);
 
-        glitch_text = myText.AddComponent<Text>();
-        glitch_text.font = (Font)Resources.Load("Anton");
-        glitch_text.text = "Glitch Detected";
-        glitch_text.fontSize = 30;
-        glitch_text.color = Color.gray;
+            glitch_text = myText.AddComponent<Text>();
+            glitch_text.font = (Font)Resources.Load("Anton");
+            glitch_text.text = "Glitch Detected";
+            glitch_text.fontSize = 30;
+            glitch_text.color = Color.gray;
 
-        // Text position
-        rectTransform = glitch_text.GetComponent<RectTransform>();
-        rectTransform.localPosition = new Vector3(19.3f, -7.0f, 0);
-        rectTransform.sizeDelta = new Vector2(400, 200);
-        rectTransform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+            // Text position
+            rectTransform = glitch_text.GetComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(19.3f, -7.0f, 0);
+            rectTransform.sizeDelta = new Vector2(400, 200);
+            rectTransform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+        }
+        else
+        {
+            // Intro Text
+            myText = new GameObject();
+            myText.transform.parent = myGO.transform;
+            myText.name = "IntroText";
+            // Debug.Log("mytext.transform = " + myText.transform);
 
+            intro_text = myText.AddComponent<Text>();
+            intro_text.font = (Font)Resources.Load("Anton");
+            intro_text.text = "This is a digital logic game.  Move the switches on the left up and\ndown to activate the logic and change the state of the light bulb.\nSome levels generate glitches.  The coin counter runs until the\nlight bulb is switched or glitched, and then the current coins is\nadded to your total coins.  Earn coins to buy fabulous prizes\nand additions to the game.\n\nJohn\njgm23333@gmail.com";
+            intro_text.fontSize = 36;
+
+            // Text position
+            rectTransform = intro_text.GetComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(8.0f, -6.8f, 0);
+            rectTransform.sizeDelta = new Vector2(1000, 1000);
+            rectTransform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+
+        }
     }
 
     public void OnMouseDown()
@@ -156,27 +178,34 @@ public class LevelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LightBulb _lb = FindObjectOfType<LightBulb>();
-        if (_lb.Switched() || _lb.Glitched())
+        if (_nextLevelIndex != 0)
         {
-            // Debug.Log(" in update LC, switched = " + _lb.Switched() + " glitched = " + _lb.Glitched());
 
-            if (!_scoreAdded)
+
+            LightBulb _lb = FindObjectOfType<LightBulb>();
+
+            if (_lb.Switched() || _lb.Glitched())
             {
-                _totalCoins += (int)((float)_centiCoinsThisLevel / 100.0);
-                tc_text.text = "Total Coins:  " + _totalCoins;
-                _scoreAdded = true;  
-                if (_lb.Glitched())
+                // Debug.Log(" in update LC, switched = " + _lb.Switched() + " glitched = " + _lb.Glitched());
+
+                if (!_scoreAdded)
                 {
-                    glitch_text.color = Color.white;
+                    _totalCoins += (int)((float)_centiCoinsThisLevel / 100.0);
+                    tc_text.text = "Total Coins:  " + _totalCoins;
+                    _scoreAdded = true;
+                    if (_lb.Glitched())
+                    {
+                        glitch_text.color = Color.white;
+                    }
                 }
             }
-        } else
-        {
-            if (_centiCoinsThisLevel > 0) _centiCoinsThisLevel--;
-        }
-        cc_text.text = "Current Coins:  " + (int)((float)_centiCoinsThisLevel / 100.0);
-    }
+            else
+            {
+                if (_centiCoinsThisLevel > 0) _centiCoinsThisLevel--;
+            }
+            cc_text.text = "Current Coins:  " + (int)((float)_centiCoinsThisLevel / 100.0);
 
+        }
+    }
 
 }
