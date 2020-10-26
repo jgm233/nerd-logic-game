a4f9bd34dbe031aa1fbfca1f9a97152f7cce9b12
John McBride <jgm23333@gmail.com>
2020-10-26 11:30:25 -0600

    Changed gates to be leadless

---
+++ GlobalHelp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GlobalHelp : MonoBehaviour
+{
+    private bool _global_help_on = false;
+
+    public void OnMouseDown()
+    {
+        _global_help_on = !_global_help_on;
+        Debug.Log("In " + this.name + " global help is now on: " + _global_help_on);
+    }
+
+    // Update is called once per frame
+    public bool IsGlobalHelpOn()
+    {
+        return _global_help_on;
+    }
+}
