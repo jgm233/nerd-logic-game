f41db9291625b0c047a7af27f60787391dbcfa61
John McBride <jgm23333@gmail.com>
2020-10-26 20:23:12 -0600

    Fixed inversion bug in OrGate.cs

--- OrGate.cs
+++ OrGate.cs
@@ -7,7 +7,7 @@ public class OrGate : BasicGate
 {
     public override void EvaluateGate()
     {
-        _out = !(_a | _b);
+        _out = (_a | _b);
         Debug.Log("In " + this.name + " EvaluateGate out =  " + _out);
         PropagateOutput();
     }
